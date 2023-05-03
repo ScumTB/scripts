57cd129444ec5fc753fc38dcb3f1bed948a306e4
ScumTB <981775064@qq.com>
2023-05-04 01:25:17 +0800

    del space

--- wp-change.sh
+++ wp-change.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-#feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/view
 #feh --recursive --randomize --bg-fill ~/Pictures/wallpapers
 feh --randomize --bg-fill ~/Pictures/wallpapers
