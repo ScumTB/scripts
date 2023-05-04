b78ef98b307c629a7443e16652d91a510259d449
ScumTB <981775064@qq.com>
2023-05-04 23:11:24 +0800

    new tap-click

--- autostart.sh
+++ autostart.sh
@@ -5,8 +5,8 @@
 #picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 picom --experimental-backends --config ~/github/picom/picom.conf &
 
-#/bin/bash ~/scripts/tap-to-click.sh &
-#/bin/bash ~/scripts/inverse-scroll.sh &
+/bin/bash ~/scripts/tap-to-click.sh &
+/bin/bash ~/scripts/inverse-scroll.sh &
 xfce4-power-manager &
 #xfce4-volumed-pulse &
 ~/scripts/autostart_wait.sh &
